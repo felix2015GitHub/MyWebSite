16a8203b7304a4a55f7c74e4450b738114df72f6
Felix <felix_cheng@syntrontech.com>
2016-06-16 23:40:14 +0800

    - add some npm init files.

---
+++ app.js
@@ -0,0 +1,41 @@
+
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500).json({
+            status: 'failure',
+            message: err.message,
+            stack: (typeof err.stack ==="string")?
+                err.stack.split("\n"): []
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500).json({
+        status: 'failure',
+        message: err.message
+    });
+});
+
+module.exports = app;
