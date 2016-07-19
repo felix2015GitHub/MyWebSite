5afda07d36ffbbee8e171411aa9d0c67bdba1554
Felix <felix_cheng@syntrontech.com>
2016-07-20 00:45:13 +0800

    - add travel map page.
    Conflicts:
    	index.html

---
+++ controllers/travelMapController.js
@@ -0,0 +1,65 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('website')
+        .controller('TravelMapController', TravelMapController);
+
+    /** @ngInject */
+    function TravelMapController($state, $scope) {
+        var vm = this;
+        vm.$state = $state;
+
+        $scope.options = {
+            chart: {
+                type: 'pieChart',
+                height: 500,
+                x: function(d){return d.key;},
+                y: function(d){return d.y;},
+                showLabels: true,
+                duration: 500,
+                labelThreshold: 0.01,
+                labelSunbeamLayout: true,
+                legend: {
+                    margin: {
+                        top: 5,
+                        right: 35,
+                        bottom: 5,
+                        left: 0
+                    }
+                }
+            }
+        };
+
+        $scope.data = [
+            {
+                key: "One",
+                y: 5
+            },
+            {
+                key: "Two",
+                y: 2
+            },
+            {
+                key: "Three",
+                y: 9
+            },
+            {
+                key: "Four",
+                y: 7
+            },
+            {
+                key: "Five",
+                y: 4
+            },
+            {
+                key: "Six",
+                y: 3
+            },
+            {
+                key: "Seven",
+                y: .5
+            }
+        ];
+    }
+})();
