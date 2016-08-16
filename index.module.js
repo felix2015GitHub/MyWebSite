0be0885f90c688db66473f4fca69d957d6b399c9
Felix <felix_cheng@syntrontech.com>
2016-08-16 15:02:21 +0800

    - remove d3 from index

--- index.module.js
+++ index.module.js
@@ -1,5 +1,5 @@
 (function() {
   'use strict';
   angular
-  	.module('website', ['ui.router', 'ui.bootstrap', 'ngSanitize', 'btford.markdown', 'nvd3']);
+  	.module('website', ['ui.router', 'ui.bootstrap', 'ngSanitize', 'btford.markdown']);
 })();
